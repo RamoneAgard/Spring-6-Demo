072a68cbe262435ebbcc6850caf02d02db83ccdb
RamoneAgard <rhagard@wustl.edu>
2023-03-17 15:05:02 -0500

    initial commit

---
+++ src/main/java/agard/spring/spring6demo/Spring6DemoApplication.java
@@ -0,0 +1,13 @@
+package agard.spring.spring6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agard/spring/spring6demo/Spring6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package agard.spring.spring6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
